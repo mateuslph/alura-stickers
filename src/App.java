8fa9005040dd207041c88a1eedebcd97e7ca118f
mateuslph <mlptche@gmail.com>
2022-07-18 16:53:20 -0300

    API Inicializada

---
+++ src/App.java
@@ -0,0 +1,39 @@
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // fazer conexão e buscar os top 250 filmes
+
+        String url = "https://api.mocki.io/v2/549a5d8b";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        // extrair (parsear os dados) soh os dados que interessam (titulo, poster e classificacao)
+
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
+        // exibir e manipular os dados
+
+        for (Map<String,String> filme : listaDeFilmes) {
+
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+            
+        }
+
+    }
+}
